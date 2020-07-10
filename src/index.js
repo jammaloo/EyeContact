97b55b2d56725398c4d146e3c2ff1ed675df0999
James Freeman <jmcfreeman@gmail.com>
2020-07-09 21:00:15 -0400

    Add matter, with POC

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-function component() {
-  const element = document.createElement('div');
+import Matter from 'matter-js'
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello World';
+// module aliases
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
-  return element;
-}
+// create an engine
+var engine = Engine.create();
 
-document.body.appendChild(component());
+// create a renderer
+var render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+  }
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
