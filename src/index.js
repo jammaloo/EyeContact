b436e91e98b13cd7213d2f2e963dc1a56898ef77
James Freeman <jmcfreeman@gmail.com>
2020-07-13 10:32:20 -0400

    Eyeball cradle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import Matter from 'matter-js'
-import Eye from './eye.png';
+import Eye1 from './eye100-1.png';
+import Eye2 from './eye100-2.png';
+import Eye3 from './eye100-3.png';
+import Eye4 from './eye100-4.png';
+import Eye5 from './eye100-5.png';
+import Eye6 from './eye100-6.png';
+
+var eyes = [Eye1, Eye2, Eye3, Eye4, Eye5, Eye6]
 
 var Engine = Matter.Engine,
   Render = Matter.Render,
   Runner = Matter.Runner,
   Body = Matter.Body,
   Bodies = Matter.Bodies,
-  Composites = Matter.Composites,
   Composite = Matter.Composite,
   Constraint = Matter.Constraint,
   MouseConstraint = Matter.MouseConstraint,
@@ -22,9 +28,9 @@ var render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
-    background: '#0f0f13',
+    width: 1000,
+    height: 800,
+    background: '#90b4ea',
     showAngleIndicator: false,
     wireframes: false
   }
@@ -39,15 +45,22 @@ Runner.run(runner, engine);
 function newtonsCradle(xx, yy, number, size, length) {
   var newtonsCradle = Composite.create({ label: 'Newtons Cradle' });
 
+  var scale = size / 50;
+
+  var eyeOffset = Math.floor(Math.random() * eyes.length)
+
   for (var i = 0; i < number; i++) {
+    var Eye = eyes[(eyeOffset + i) % eyes.length]
     var separation = 1.9,
       circle = Bodies.circle(xx + i * (size * separation), yy + length, size,
-        { inertia: Infinity, restitution: 1, friction: 0, frictionAir: 0.0001, slop: 1, render: {
+        { inertia: Infinity, restitution: 1, friction: 0, frictionAir: 0, slop: 1, render: {
             sprite: {
               texture: Eye,
+              xScale: scale,
+              yScale: scale,
             }
           } }),
-      constraint = Constraint.create({ pointA: { x: xx + i * (size * separation), y: yy }, bodyB: circle });
+      constraint = Constraint.create({ pointA: { x: xx + i * (size * separation), y: yy }, bodyB: circle, render: { anchors: false, lineWidth: 0, strokeStyle: '#ff0000' } });
 
     Composite.addBody(newtonsCradle, circle);
     Composite.addConstraint(newtonsCradle, constraint);
@@ -57,11 +70,11 @@ function newtonsCradle(xx, yy, number, size, length) {
 };
 
 // add bodies
-var cradle = newtonsCradle(280, 100, 5, 30, 200);
+var cradle = newtonsCradle(280, 100, 5, 60, 200);
 World.add(world, cradle);
 Body.translate(cradle.bodies[0], { x: -180, y: -100 });
 
-cradle = newtonsCradle(280, 380, 7, 20, 140);
+cradle = newtonsCradle(280, 380, 7, 40, 140);
 World.add(world, cradle);
 Body.translate(cradle.bodies[0], { x: -140, y: -100 });
 
@@ -85,5 +98,5 @@ render.mouse = mouse;
 // fit the render viewport to the scene
 Render.lookAt(render, {
   min: { x: 0, y: 50 },
-  max: { x: 800, y: 600 }
+  max: { x: 1000, y: 600 }
 });
